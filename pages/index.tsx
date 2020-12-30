54bad6551035da2c6f03238b48fa80d87b6a66f2
Denis Savin <ds@openstart.ru>
2020-12-30 04:24:49 +0300

    add ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+const utilStyles = require('../styles/utils.module.css')
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async (context) => {
+  console.log(context)
   const allPostsData = getSortedPostsData()
   return {
     props: {
